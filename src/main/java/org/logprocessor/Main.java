e0c4492c84beef89dd137e14f70caa7b104a48f9
ayoubben18 <ayoubbensalah2004@gmail.com>
2025-12-23 01:28:41 +0100

    Making the logger multithreaded and safe

--- src/main/java/org/logprocessor/Main.java
+++ src/main/java/org/logprocessor/Main.java
@@ -3,27 +3,47 @@ package org.logprocessor;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     void main() {
-        Path path = Path.of("data.csv");
+        AtomicInteger grandTotal = new AtomicInteger(0);
+        List<String> files = List.of("data1.csv", "data2.csv", "data3.csv");
+
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (String file : files) {
+                executor.submit(() -> {
+                    int totalTime = getTotalErrorTime(file);
+                    // thread safe
+                    grandTotal.addAndGet(totalTime);
+                    IO.println(file + " processed. Total error time: " + totalTime);
+                });
+            }
+        }
+
+        IO.println("All logs are processed");
+    }
+
+    private int getTotalErrorTime(String fileName) {
+        Path path = Path.of(fileName);
 
         try (Stream<String> lines = Files.lines(path)) {
-            double average = lines
+            return lines
                     .map(this::parseLine)
                     .flatMap(Optional::stream)
                     .filter(log -> log.level() == LogLevel.ERROR)
                     .mapToInt(LogEntry::responseTime)
-                    .average()
-                    .orElse(0.0);
-
-            IO.println("Average error response time : " + average);
+                    .sum();
         } catch (IOException e) {
-            IO.println("Could not read file: " + e.getMessage());
+            IO.println("Error reading " + fileName + ":" + e.getMessage());
+            return 0;
         }
     }
 
